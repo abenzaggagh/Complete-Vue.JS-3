58ed862fad45db7de49f86148b31c47a24505f22
BEN ZAGGAGH <amine.ben-zaggagh@capgemini.com>
2025-03-31 14:54:08 +0200

    Store caching

--- capstone/src/store/photos.js
+++ capstone/src/store/photos.js
@@ -2,22 +2,29 @@ export const photos = {
     namespaced: true,
     state() {
         return {
-            all: []
+            all: [],
+            cache: {
+
+            }
         }
     },
     mutations: {
-        setPhotosForCurrentAlbum(state, photos) {
+        setPhotosForCurrentAlbum(state,  {photos, albumId}) {
             state.all = photos;
+            state.cache[albumId] = photos;
         }
     },
     actions: {
         async getByAlbum(context, { albumId }) {
+            if (context.state.cache[albumId]) {
+                context.commit('setPhotosForCurrentAlbum', {photos: context.state.cache[albumId], albumId });
+            }
+            
             const response = await fetch(`https://jsonplaceholder.typicode.com/photos?album=${albumId}`);
 
             const json = await response.json();
             
-            console.log(json);
-            context.commit('setPhotosForCurrentAlbum', json);
+            context.commit('setPhotosForCurrentAlbum', {photos: json, albumId });
         }
     }
 }
